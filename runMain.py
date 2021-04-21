1b6cf4458f78892f8761f4670472db0f30bd9cf4
hcs <8438964+hcs123@user.noreply.gitee.com>
2021-04-21 23:45:10 +0800

    Emotion recognition project for hcs team

---
+++ runMain.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+from EmotionRecongnition import Ui_MainWindow
+from sys import argv, exit
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(argv)
+
+    window = QMainWindow()
+    ui = Ui_MainWindow(window)
+
+    window.show()
+    exit(app.exec_())
